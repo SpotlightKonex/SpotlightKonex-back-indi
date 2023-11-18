e9d28f3241bfe4cfa1223677f1fa477c64d7bdca
2522001 <minseo770@gmail.com>
2023-11-18 16:28:12 +0900

    [Feat] #1 - ReceiveData 함수 정의

--- konexIndi.py
+++ konexIndi.py
@@ -2,6 +2,9 @@ import sys
 import GiExpertControl as giJongmokTRShow
 from indiUI import Ui_MainWindow
 
+currentData = []
+previousDayData = []
+
 class konexIndi():
 
     def __init__(self):
@@ -10,7 +13,7 @@ class konexIndi():
         giJongmokTRShow.SetCallBack('ReceiveData', self.giJongmokTRShowReceiveData)
         self.rqidD = {}
 
-    # konexCurrentData - 표준코드, 현재가, 전일대비율, 누적거래대금
+    # [VC] input: 표준코드
 
     def getKonexCurrentData(self, standardCode):
         
@@ -25,12 +28,12 @@ class konexIndi():
         print('Request Data rqid: ' + str(rqid))
         self.rqidD[rqid] = TRName
 
-    # konexPreviousDayData - 전일종가, 전일누적체결수량
+    # [VB] input: 표준코드
 
     def getKonexPreviousDayData(self, standardCode):
         
         print("get konex details")
-        TRName = "VC"
+        TRName = "VB"
 
         ret = giJongmokTRShow.SetQueryName(TRName)          
         ret = giJongmokTRShow.SetSingleData(0, standardCode) # 표준코드
@@ -39,3 +42,48 @@ class konexIndi():
         print(type(rqid))
         print('Request Data rqid: ' + str(rqid))
         self.rqidD[rqid] = TRName
+
+    def giJongmokTRShow_ReceiveData(self,giCtrl,rqid):
+    
+        print("in receive_Data:",rqid)
+        print('recv rqid: {}->{}\n'.format(rqid, self.rqidD[rqid]))
+        TRName = self.rqidD[rqid]
+        print("TR_name : ",TRName)
+
+        # [VC] output: 표준코드(0), 현재가(3), 전일대비율(6), 누적거래대금(8)
+
+        if TRName == "VC":
+
+            nCnt = giCtrl.GetSingleRowCount()
+            print("받아온 데이터의 개수: ", nCnt)
+
+            for i in range(nCnt):
+                standardCode = str(giCtrl.GetSingleData(i, 0)) # 표준코드
+                currentPrice = str(giCtrl.GetSingleData(i, 3)) # 현재가
+                changeRate = str(giCtrl.GetSingleData(i, 6)) # 전일대비율
+                volume = str(giCtrl.GetSingleData(i, 8)) # 누적거래대금
+
+                currentData.append([standardCode, currentPrice, changeRate, volume])
+        
+        # [VC] output: 표준코드(0), 전일종가(11), 전일누적체결수량(16)
+
+        if TRName == "VB":
+
+            nCnt = giCtrl.GetSingleRowCount()
+            print("받아온 데이터의 개수: ", nCnt)
+
+            for i in range(nCnt):
+                standardCode = str(giCtrl.GetSingleData(i, 0)) # 표준코드
+                previousDayClosingPrice = str(giCtrl.GetSingleData(i, 11)) # 전일종가
+                previousDayQuantity = str(giCtrl.GetSingleData(i, 16)) # 전일누적체결수량
+
+                currentData.append([standardCode, currentPrice, changeRate, volume])
+
+
+                
+            
+
+
+
+
+            
