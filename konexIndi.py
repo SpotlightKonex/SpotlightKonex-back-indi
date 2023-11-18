e5ecb5f60e644b529e70d1010814ae58b3126d81
2522001 <minseo770@gmail.com>
2023-11-18 16:48:54 +0900

    [Feat] #1 - getKonexStandardCode 함수 정의

--- konexIndi.py
+++ konexIndi.py
@@ -1,8 +1,9 @@
 import GiExpertControl as giLogin
 import GiExpertControl as giJongmokTRShow
 
-currentData = []
-previousDayData = []
+standardCodeList = []
+currentDataList = []
+previousDayDataList = []
 
 class konexIndi():
 
@@ -54,6 +55,22 @@ class konexIndi():
         print('Request Data rqid: ' + str(rqid))
         self.rqidD[rqid] = TRName
 
+    # [knx_mst] input: None
+
+    def getKonexStandardCode(self):
+
+        print("get konex standard code")
+        TRName = "knx_mst"
+
+        ret = giJongmokTRShow.SetQueryName(TRName)          
+        rqid = giJongmokTRShow.RequestData()
+
+        print(type(rqid))
+        print('Request Data rqid: ' + str(rqid))
+        self.rqidD[rqid] = TRName
+
+    # output
+
     def giJongmokTRShow_ReceiveData(self,giCtrl,rqid):
     
         print("in receive_Data:",rqid)
@@ -74,9 +91,9 @@ class konexIndi():
                 changeRate = str(giCtrl.GetSingleData(i, 6)) # 전일대비율
                 volume = str(giCtrl.GetSingleData(i, 8)) # 누적거래대금
 
-                currentData.append([standardCode, currentPrice, changeRate, volume])
+                currentDataList.append([standardCode, currentPrice, changeRate, volume])
         
-        # [VC] output: 표준코드(0), 전일종가(11), 전일누적체결수량(16)
+        # [VB] output: 표준코드(0), 전일종가(11), 전일누적체결수량(16)
 
         if TRName == "VB":
 
@@ -88,8 +105,20 @@ class konexIndi():
                 previousDayClosingPrice = str(giCtrl.GetSingleData(i, 11)) # 전일종가
                 previousDayQuantity = str(giCtrl.GetSingleData(i, 16)) # 전일누적체결수량
 
-                previousDayData.append([standardCode, previousDayClosingPrice, previousDayQuantity])
+                previousDayDataList.append([standardCode, previousDayClosingPrice, previousDayQuantity])
 
+        # [knx_mst] ouput: 표준코드(0)
+
+        if TRName == "knx_mst":
+
+            nCnt = giCtrl.getMultiRowCount()
+            print("받아온 데이터의 개수: ", nCnt)
+
+            for i in range(nCnt):
+                standardCode = str(giCtrl.GetSingleData(i, 0)) # 표준코드
+                standardCodeList.append(standardCode)
+            
+            print("StandardCodeList: ", standardCodeList)
 
                 
             
